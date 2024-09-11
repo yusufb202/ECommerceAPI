94ddf9c5a31d8d9968f04a02e810e004d09b6619
Yusuf Bayram <yusufb202@gmail.com>
2024-09-11 09:13:57 +0300

    1

--- Repository/AppDbContext.cs
+++ Repository/AppDbContext.cs
@@ -42,7 +42,17 @@ namespace Repository
                 new Category { Id = 1, Name = "Default" }
                 );
 
-
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 4, Name = "T-Shirt 1", Description = "Description 1", Price = 11, Stock = 21, CategoryId = 5 },
+                new Product { Id = 5, Name = "T-Shirt 2", Description = "Description 2", Price = 12, Stock = 22, CategoryId = 5 },
+                new Product { Id = 6, Name = "T-Shirt 3", Description = "Description 3", Price = 13, Stock = 23, CategoryId = 5 },
+                new Product { Id = 7, Name = "T-Shirt 4", Description = "Description 4", Price = 14, Stock = 24, CategoryId = 5 },
+                new Product { Id = 8, Name = "T-Shirt 5", Description = "Description 5", Price = 15, Stock = 25, CategoryId = 5 },
+                new Product { Id = 9, Name = "T-Shirt 6", Description = "Description 6", Price = 16, Stock = 26, CategoryId = 5 },
+                new Product { Id = 10, Name = "T-Shirt 7", Description = "Description 7", Price = 17, Stock = 27, CategoryId = 5 },
+                new Product { Id = 11, Name = "T-Shirt 8", Description = "Description 8", Price = 18, Stock = 28, CategoryId = 5 },
+                new Product { Id = 12, Name = "T-Shirt 9", Description = "Description 9", Price = 19, Stock = 29, CategoryId = 5 }
+                );
 
             modelBuilder.Entity<Category>()
              .HasMany(c => c.Products)
---
+++ Repository/Migrations/20240911060300_SeedProduct.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 4, 5, "Description 1", "T-Shirt 1", 11m, 21 },
+                    { 5, 5, "Description 2", "T-Shirt 2", 12m, 22 },
+                    { 6, 5, "Description 3", "T-Shirt 3", 13m, 23 },
+                    { 7, 5, "Description 4", "T-Shirt 4", 14m, 24 },
+                    { 8, 5, "Description 5", "T-Shirt 5", 15m, 25 },
+                    { 9, 5, "Description 6", "T-Shirt 6", 16m, 26 },
+                    { 10, 5, "Description 7", "T-Shirt 7", 17m, 27 },
+                    { 11, 5, "Description 8", "T-Shirt 8", 18m, 28 },
+                    { 12, 5, "Description 9", "T-Shirt 9", 19m, 29 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12);
+        }
+    }
+}
